122d17eea22d0a105c70e24343afd3a389c15856
GusLpz <91474994+GusLpz@users.noreply.github.com>
2025-04-12 15:17:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 import numpy as np
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
 
 
 # Configiracion de la pagina
